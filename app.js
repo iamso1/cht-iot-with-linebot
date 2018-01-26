3984e33209ca0cb78445ee7c3120c32a7e96b51b
Hsinhan <bmhiamso1@gmail.com>
2018-01-26 21:02:59 +0800

    完成簡易linebot範例

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const linebot = require('linebot');
 const express = require('express');
-
-const bot = linebot({
-  channelId: '',
-  channelSecret: '',
-  channelAccessToken: '',
-});
+const linebot = require('linebot');
+const lineConfig = require('./linebot-setting');
+const bot = linebot(lineConfig);
 
 const app = express();
 const botParser = bot.parser();
@@ -14,18 +10,28 @@ const botParser = bot.parser();
 app.post('/', botParser);
 
 app.get('/', function(req, res) {
-  res.statusCode = 200;
   res.send('hello world');
 });
 
 //這邊的 process.env.PORT  會對應到 heroku 的 80
-
 const server = app.listen(process.env.PORT || 3000, function() {
   const port = server.address().port;
   console.log('Server now running on port', port);
 });
 
+//主要監聽的部分
 bot.on('message', function(event) {
-  //這邊會監聽line打過來的資料
-  console.log(event);
+  if ((event.message.type = 'text')) {
+    var msg = event.message.text;
+    event
+      .reply(msg)
+      .then(function(data) {
+        // success
+        console.log(msg);
+      })
+      .catch(function(error) {
+        // error
+        console.log('error');
+      });
+  }
 });
